cf0d58b745b12467b107f84e04e498f46ef04895
schat.top <49747805+www-schat-top@users.noreply.github.com>
2019-05-10 19:21:37 +0800

    Create cidr2range.php

---
+++ PHP/cidr2range.php
@@ -0,0 +1,10 @@
+/**
+ * convert cidr to ip range
+ * From: <https://github.com/www-schat-top>
+ * @param  string $cidr "192.168.0.0/23"
+ * @return array  ["192.168.0.0", "192.168.1.255"]
+ */
+function cidr2range($cidr){
+   list( $subnet, $mask ) = explode( '/', $cidr );
+   return [long2ip($subnet), long2ip(ip2long( $subnet ) | (pow(2,( 32 - $mask ))-1))];
+}
