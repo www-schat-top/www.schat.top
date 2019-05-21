ae9ed23f66b5ca66123d7a07be623f4c5263c578
schat.top <49747805+www-schat-top@users.noreply.github.com>
2019-05-21 23:44:35 +0800

    Update cidr2range.php

--- PHP/cidr2range.php
+++ PHP/cidr2range.php
@@ -1,6 +1,7 @@
 /**
  * convert cidr to ip range
  * From: <https://github.com/www-schat-top>
+ * Link: https://www.php.net/manual/zh/function.ip2long.php
  * @param  string $cidr "192.168.0.0/23"
  * @return array  ["192.168.0.0", "192.168.1.255"]
  */
